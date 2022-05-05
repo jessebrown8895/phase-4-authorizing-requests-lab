5275f6533a1dbc17a4b7e19c24a44960c5524382
Jesse Brown <Brownjesse8895@yahoo.com>
2022-05-05 18:42:53 -0500

    commit tree ee9893bcafea7b0d0be5e7b199a7f650e8e81661

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
   
+
+  
   def create
     user = User.find_by(username: params[:username])
     session[:user_id] = user.id
@@ -10,5 +12,8 @@ class SessionsController < ApplicationController
     session.delete :user_id
     head :no_content
   end
+  
+  
 
+  
 end
